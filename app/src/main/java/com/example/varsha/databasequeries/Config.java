5399ae48137d68bd7f0e0057ac7a97ff3003a24c
Varsha Jambunathan <jvarsha621@gmail.com>
2016-11-08 23:06:59 +0530

    Adding dependencies

---
+++ app/src/main/java/com/example/varsha/databasequeries/Config.java
@@ -0,0 +1,9 @@
+package com.example.varsha.databasequeries;
+
+/**
+ * Created by Varsha on 08-11-2016.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://databasequeries-30365.firebaseio.com/";
+}
